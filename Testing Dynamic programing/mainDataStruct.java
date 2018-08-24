0888c9fe8db82308a1f118e26909317a2c56c0c6
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-08-23 23:31:02 -0500

    Testing dynamic programming speeds

---
+++ Testing Dynamic programing/mainDataStruct.java
@@ -0,0 +1,12 @@
+
+
+
+//class used for running main
+public class mainDataStruct 
+{
+	public static void main(String[] args)
+	{
+		FibonacciRunTime fib = new FibonacciRunTime();
+		fib.runtimes();
+	}
+}

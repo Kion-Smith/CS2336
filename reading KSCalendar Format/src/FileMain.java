142e4f61125f1894bab38466caf2f23eb023d10e
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-09-10 21:53:05 -0500

    Changing way I save the file

--- reading KSCalendar Format/src/FileMain.java
+++ reading KSCalendar Format/src/FileMain.java
@@ -29,75 +29,80 @@ public class FileMain
 			System.out.println("");
 			
 			String curLine;
-			boolean lookingForBrace = false;
-			boolean lookingForEndBrace = false;
+
 			while( (curLine =bf.readLine()) != null)
 			{
 				System.out.println(curLine);
 				
-				for(int i =0; i< curLine.length();i++)
+				try
 				{
-				
-					try
+					if((curLine.charAt(0)+"").equals("["))
 					{
-						if(i==0 && (curLine.charAt(i)+"").equals("["))
-						{
-							// could put this in a date handling method
-							String yearString =curLine.substring(curLine.indexOf("[")+1,curLine.indexOf("/"));
+							// could put this in a date handling all string items. Could also make 2 methods for handeling string items
+						String yearString =curLine.substring(curLine.indexOf("[")+1,curLine.indexOf("/"));
 								
-							String monthString =curLine.substring(curLine.indexOf("/")+1);
-							String dayString = monthString;
-							monthString = monthString.substring(0,monthString.indexOf("/"));
+						String monthString =curLine.substring(curLine.indexOf("/")+1);
+						String dayString = monthString;
+						monthString = monthString.substring(0,monthString.indexOf("/"));
+							
+						String dataItems = dayString;
+						dayString = dayString.substring(dayString.indexOf("/")+1,dayString.indexOf("]"));
 								
-							dayString = dayString.substring(dayString.indexOf("/")+1,dayString.indexOf("]"));
-								
-							System.out.println("@ Output - Year = "+yearString);
-							System.out.println("@ Output - Month = "+monthString);
-							System.out.println("@ Output - Day = "+dayString);
+						System.out.println("@ Output - Year = "+yearString);
+						System.out.println("@ Output - Month = "+monthString);
+						System.out.println("@ Output - Day = "+dayString);
 							
 							
+							//changed idea only valid way of entering data for a month is
+						clArray.add( new CalendarObject(convertStringToInt(yearString),convertStringToInt(monthString),convertStringToInt(dayString)));
+						dataItems = dataItems.substring(dataItems.indexOf("{"));
 							
-							clArray.add( new CalendarObject(convertStringToInt(yearString),convertStringToInt(monthString),convertStringToInt(dayString)));
-							
-							/*Pseudo code for getting info
-							 * 
-							 * if has :{ } in string keep add those items
-							 * else if has : but not { set looking for start brace boolean to true and then start looking for the end
-							 * else return and error and break
-							 */
-							
-							//tempary for testing
-							lookingForBrace = true;
-							
-							
-						}
-						
-						if(lookingForBrace &&  (curLine.charAt(i)+"").equals("{"))
+						boolean hasRightBrace = false;
+						boolean hasFirstQuote= false;
+						String temp ="";
+						for(int i=0;i<dataItems.length();i++)
 						{
-							System.out.println(" Start: Looking for brace > "+curLine);
-							lookingForBrace = false;
-							lookingForEndBrace = true;
+							//System.out.println("DEBUG::"+(dataItems.charAt(i)+""));
+							switch( (dataItems.charAt(i)+""))
+							{
+								case "{":
+									if(!hasRightBrace)
+									{
+										hasRightBrace = true;
+									}
+									break;
+								case "\"":
+									if(hasRightBrace && !hasFirstQuote)
+									{
+										hasFirstQuote = true;
+										break;
+									}
+									else if(hasRightBrace && hasFirstQuote)
+									{
+										break;
+									}
+									
+								default:
+									temp += dataItems.charAt(i)+"";
+									System.out.println("DID THIS RUN:: "+temp);
+									break;
+							}
 						}
-						/* problem here
-						if(lookingForEndBrace &&  (curLine.contains('"'+"")) );
-						{
-							System.out.println(" Content: Looking for content > "+curLine);
-						} */
-						if(lookingForEndBrace &&  (curLine.charAt(i)+"").equals("}"))
-						{
-							System.out.println(" End: Looking for end > "+curLine);
-						}
-						
+
+							
 					}
-					catch(Exception e)
+					else
 					{
 						break;
 					}
-
 					
-				}//end for
-				
-				
+				}
+				catch(Exception e)
+				{
+					System.out.println("Error");
+					break;
+				} //end of try catch
+
 			}//end while
 
 			
